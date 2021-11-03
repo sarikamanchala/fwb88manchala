1770932c25ec61d93c330b57ed5e35fd486162eb
Manchalasarika <sarikamanchala33@gmail.com>
2021-11-02 22:43:02 -0500

    add

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var catRouter = require('./routes/cat');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var addmodsRouter = require('./routes/addmods');
 var app = express();
 
 // view engine setup
@@ -18,10 +18,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use('/cat', catRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/addmods', addmodsRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
