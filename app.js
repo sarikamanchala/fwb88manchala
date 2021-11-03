ac127ef13d3f28cadd589a6202d550514448a027
Manchalasarika <sarikamanchala33@gmail.com>
2021-11-02 23:32:19 -0500

    updated

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var catRouter = require('./routes/cat');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
 var app = express();
 
 // view engine setup
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/selector', selectorRouter);
 app.use('/cat', catRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
