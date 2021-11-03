60d2e8ffa1318c0ae98bededf1057a82eea0beb3
Manchalasarika <sarikamanchala33@gmail.com>
2021-11-03 02:11:18 -0500

    change

--- routes/cat.js
+++ routes/cat.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('cat', { title: 'Express' });
+  res.render('cat', { title: 'Search results cat' });
 });
 
 module.exports = router;
